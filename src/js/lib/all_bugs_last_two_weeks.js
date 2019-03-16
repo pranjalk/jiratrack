7027af8a2c05b26b843f7e5c7a6fd5a6acd469b6
Pranjal Kumar <pranjal.kr94@gmail.com>
2019-03-17 01:44:30 +0530

    Add three computations

--- src/js/lib/all_bugs_last_two_weeks.js
+++ src/js/lib/all_bugs_last_two_weeks.js
@@ -1,20 +1,161 @@
 import chalk from 'chalk';
+import moment from 'moment';
+
 import ApiClient from '../apiclient/index.js';
 import parameters from '../../../config/parameters.js';
 import { JIRA_SEARCH } from '../constants/url_endpoints.js';
+import { MAJOR, MINOR, CRITICAL } from '../constants/priorities.js';
 
 export default class BugsInLastTwoWeeks {
   constructor() {
     this.jqlQuery = `project = ${parameters.JIRA_BOARD} AND issuetype = Bug AND createdDate > startOfWeek(-2w) ORDER BY created DESC`;
     this.run = this.run.bind(this);
-    this.bugPriorityClassification = this.bugPriorityClassification.bind(this);
+    this.resolvedVsUnresolved = this.resolvedVsUnresolved.bind(this);
+    this.resolutionTimeFrame = this.resolutionTimeFrame.bind(this);
   }
 
-  bugPriorityClassification(data) {
+  resolvedVsUnresolved({ issues }) {
+    console.log(chalk.green('Resolved vs Unresolved in the last two weeks'));
+    console.table({
+      resolved: {
+        count: issues.filter(issue => issue.fields.resolutiondate !== null).length
+      },
+      unresolved: {
+        count: issues.filter(issue => issue.fields.resolutiondate === null).length
+      },
+      total: {
+        count: issues.length
+      }
+    });
+  }
 
+  resolvedUnresolvedPriority({ issues }) {
+    console.log(chalk.green('Resolved vs Unresolved in the last two weeks'));
+    console.table({
+      critical: {
+        resolved: issues.filter(issue => {
+          const { fields } = issue;
+          return fields.resolutiondate !== null && fields.priority.name === CRITICAL;
+        }).length,
+        unresolved: issues.filter(issue => {
+          const { fields } = issue;
+          return fields.resolutiondate === null && fields.priority.name === CRITICAL;
+        }).length
+      },
+      major: {
+        resolved: issues.filter(issue => {
+          const { fields } = issue;
+          return fields.resolutiondate !== null && fields.priority.name === MAJOR;
+        }).length,
+        unresolved: issues.filter(issue => {
+          const { fields } = issue;
+          return fields.resolutiondate === null && fields.priority.name === MAJOR;
+        }).length
+      },
+      minor: {
+        resolved: issues.filter(issue => {
+          const { fields } = issue;
+          return fields.resolutiondate !== null && fields.priority.name === MINOR;
+        }).length,
+        unresolved: issues.filter(issue => {
+          const { fields } = issue;
+          return fields.resolutiondate === null && fields.priority.name === MINOR;
+        }).length
+      },
+      total: {
+        resolved: issues.filter(issue => {
+          const { fields } = issue;
+          return fields.resolutiondate !== null && [MAJOR, MINOR, CRITICAL].includes(fields.priority.name);
+        }).length,
+        unresolved: issues.filter(issue => {
+          const { fields } = issue;
+          return fields.resolutiondate === null && [MAJOR, MINOR, CRITICAL].includes(fields.priority.name);
+        }).length
+      }
+    });
+  }
+
+  resolutionTimeFrame({ issues }) {
+    const consoleTable = {
+      [CRITICAL]: {
+        system_restore: 0,
+        perm_fix: 0,
+        exceeded: 0
+      },
+      [MAJOR]: {
+        system_restore: 0,
+        perm_fix: 0,
+        exceeded: 0
+      },
+      [MINOR]: {
+        system_restore: 0,
+        perm_fix: 0,
+        exceeded: 0
+      },
+      TOTAL: {
+        system_restore: 0,
+        perm_fix: 0,
+        exceeded: 0
+      }
+    };
+
+    issues.forEach(issue => {
+      const { fields } = issue;
+      if (fields.resolutiondate) {
+        const created = moment(fields.created);
+        const resolved = moment(fields.resolutiondate);
+        switch (fields.priority.name) {
+        case CRITICAL: {
+          if (resolved.isBefore(created.add(4, 'hours'))) {
+            consoleTable[CRITICAL].system_restore += 1;
+            consoleTable.TOTAL.system_restore += 1;
+          } else if (resolved.isBefore(created.add(96, 'hours'))) {
+            consoleTable[CRITICAL].perm_fix += 1;
+            consoleTable.TOTAL.perm_fix += 1;
+          } else {
+            consoleTable[CRITICAL].exceeded += 1;
+            consoleTable.TOTAL.exceeded += 1;
+          }
+          break;
+        }
+        case MAJOR: {
+          if (resolved.isBefore(created.add(48, 'hours'))) {
+            consoleTable[MAJOR].system_restore += 1;
+            consoleTable.TOTAL.system_restore += 1;
+          } else if (resolved.isBefore(created.add(10, 'days'))) {
+            consoleTable[MAJOR].perm_fix += 1;
+            consoleTable.TOTAL.perm_fix += 1;
+          } else {
+            consoleTable[MAJOR].exceeded += 1;
+            consoleTable.TOTAL.exceeded += 1;
+          }
+          break;
+        }
+        case MINOR: {
+          if (resolved.isBefore(created.add(5, 'days'))) {
+            consoleTable[MINOR].system_restore += 1;
+            consoleTable.TOTAL.system_restore += 1;
+          } else if (resolved.isBefore(created.add(30, 'days'))) {
+            consoleTable[MINOR].perm_fix += 1;
+            consoleTable.TOTAL.perm_fix += 1;
+          } else {
+            consoleTable[MINOR].exceeded += 1;
+            consoleTable.TOTAL.exceeded += 1;
+          }
+          break;
+        }
+        default: {
+          break;
+        }
+        }
+      }
+    });
+    console.log(chalk.green('SLA Bucket Calculations for Resolved issues in the last two weeks'));
+    console.table(consoleTable);
   }
 
   run() {
+    console.log(`${chalk.yellow('Fetching Data for Board: ')} ${chalk.bgGreen(`${parameters.JIRA_BOARD}`)}`);
     ApiClient.post(JIRA_SEARCH, {}, {
       jql: this.jqlQuery,
       validateQuery: 'warn',
@@ -32,11 +173,18 @@ export default class BugsInLastTwoWeeks {
         'created'
       ]
     }).then((res) => {
-      console.log(JSON.stringify(res.data));
+      console.log(`${chalk.green('Fetched Data Successfully: \n')}`);
+      this.resolvedVsUnresolved(res.data);
+      this.resolvedUnresolvedPriority(res.data);
+      try {
+        this.resolutionTimeFrame(res.data);
+      } catch (err) {
+        console.log(err);
+      }
     }).catch((err) => {
       console.log(chalk.red.inverse('API CALL FAILED!'));
     }).finally(() => {
-      console.log(chalk.green('Completed Successfully'));
+      console.log(chalk.green('Report Generated Successfully'));
     });
   }
 }
